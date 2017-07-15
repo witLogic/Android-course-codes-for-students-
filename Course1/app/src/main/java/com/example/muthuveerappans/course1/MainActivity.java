117265a32438a2c1af9041ec18f4d63ea9c16c13
swaaminathanm <swaaminathanm@gmail.com>
2017-07-15 16:54:12 +0530

    Added homework

--- Course1/app/src/main/java/com/example/muthuveerappans/course1/MainActivity.java
+++ Course1/app/src/main/java/com/example/muthuveerappans/course1/MainActivity.java
@@ -8,6 +8,19 @@ import android.support.v7.app.AppCompatActivity;
  * Created by muthuveerappans on 7/14/17.
  */
 
+/*
+Try and see:
++ Create a simple portfolio page of yours with the following content and attributes:
++ Place your name in the top left of the page.
++ Make your name bold.
++ Place your contact number below your name
++ Email address below the contact number.
++ Place your passport size photo on the top right corner of the page.
++ Enter few lines about yourself.
++ The background color of the page should be #ededed.
++ All text should be in black.
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override

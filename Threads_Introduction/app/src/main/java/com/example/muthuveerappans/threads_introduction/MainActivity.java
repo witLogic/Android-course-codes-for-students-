5dcd5e9901de494f5a0c128ab4238941842a30df
swaaminathanm <swaaminathanm@gmail.com>
2017-08-05 12:24:31 +0530

    Added Asynctask

--- Threads_Introduction/app/src/main/java/com/example/muthuveerappans/threads_introduction/MainActivity.java
+++ Threads_Introduction/app/src/main/java/com/example/muthuveerappans/threads_introduction/MainActivity.java
@@ -1,27 +1,32 @@
 package com.example.muthuveerappans.threads_introduction;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView resultTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.i("test", "Main Thread :" + Thread.currentThread().getId());
+
         final EditText editText = (EditText) findViewById(R.id.editText);
-        final TextView resultTxt = (TextView) findViewById(R.id.result_text);
+        resultTxt = (TextView) findViewById(R.id.result_text);
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int number = Integer.valueOf(editText.getText().toString());
-                resultTxt.setText("" + fibonacci(number));
+                new LongOperation().execute(number);
             }
         });
 
@@ -33,15 +38,6 @@ public class MainActivity extends AppCompatActivity {
 
         t1.print();
         t2.print();
-    }
-
-    public int fibonacci(int n) {
-        if (n == 0)
-            return 0;
-        else if (n == 1)
-            return 1;
-        else
-            return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
     // static, global discussion
@@ -57,6 +53,32 @@ public class MainActivity extends AppCompatActivity {
         public void print() {
             Log.i("t", "a: " + a);
             Log.i("t", "a1: " + a1);
+        }
+    }
+
+    private class LongOperation extends AsyncTask<Integer, Void, Integer> {
+
+        public int fibonacci(int n) {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+                return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... params) {
+            Log.i("test", "Background Thread :" + Thread.currentThread().getId());
+            int number = params[0];
+            int result = fibonacci(number);
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            Log.i("test", "Main Thread :" + Thread.currentThread().getId());
+            resultTxt.setText("" + integer);
         }
     }
 }

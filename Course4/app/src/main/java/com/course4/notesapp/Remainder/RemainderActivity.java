98091a642fc678a4e68d6763f82933570d10c37c
swaaminathanm <swaaminathanm@gmail.com>
2017-09-15 02:11:45 +0530

    Added notifications and broadcast receivers

--- Course4/app/src/main/java/com/course4/notesapp/Remainder/RemainderActivity.java
+++ Course4/app/src/main/java/com/course4/notesapp/Remainder/RemainderActivity.java
@@ -12,10 +12,12 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.NavUtils;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.DateFormat;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -42,6 +44,7 @@ public class RemainderActivity extends AppCompatActivity implements View.OnClick
     Button submit, pickTime;
     EditText notesEdt;
     int hour, min;
+    boolean fromNotification = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -65,7 +68,32 @@ public class RemainderActivity extends AppCompatActivity implements View.OnClick
             pickTime.setText(new StringBuilder().append(pad(hour))
                     .append(":").append(pad(min)));
             submit.setVisibility(View.GONE);
+            fromNotification = true;
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                Intent upIntent = NavUtils.getParentActivityIntent(this);
+                if (NavUtils.shouldUpRecreateTask(this, upIntent) || fromNotification) {
+                    // This activity is NOT part of this app's task, so create a new task
+                    // when navigating up, with a synthesized back stack.
+                    TaskStackBuilder.create(this)
+                            // Add all of this activity's parents to the back stack
+                            .addNextIntentWithParentStack(upIntent)
+                            // Navigate up to the closest parent
+                            .startActivities();
+                } else {
+                    // This activity is part of this app's task, so simply
+                    // navigate up to the logical parent activity.
+                    NavUtils.navigateUpTo(this, upIntent);
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
